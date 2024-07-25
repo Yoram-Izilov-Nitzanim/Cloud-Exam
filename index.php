56e474f6795f52c83b8e901f9faebc85d3ffe3ca
Yoram-Izilov-Nitzanim <yoram.izilov@nitzanim.tech>
2024-07-25 16:54:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,47 @@
 <!DOCTYPE html>
 <html>
-  <head>
-  <title>Hello DevOps Class</title>
-  </head>
-  <body>
-    <?php
-      $host = 'database-2.cx248m4we6k7.us-east-1.rds.amazonaws.com';
-      $user = 'admin';
-      $password = '123456Yoram';
-      $dbname = 'breached';
-      $conn = mysqli_connect($host, $user, $password, $dbname);
-      if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-      }
-      $sql = "SELECT id, email, pass, add_date FROM breached_data";
-      $result = mysqli_query($conn, $sql);
-      echo "<h1>Breached</h1>";
-      echo "<table>";
-      echo "<tr><th>ID</th><th>Email</th><th>Password</th><th>Date
-      Added</th></tr>";
-      while ($row = mysqli_fetch_array($result)) {
-      echo "<tr><td>" . $row['id'] . "</td><td>" . $row['email'] . "</td><td>" .
-      $row['pass'] . "</td><td>" . $row['add_date'] . "</td></tr>";
-      }
-      echo "</table>";
-      mysqli_close($conn);
-    ?>
-  </body>
+<head>
+    <title>Hello DevOps Class</title>
+</head>
+<body>
+<?php
+$host = 'database-2.cx248m4we6k7.us-east-1.rds.amazonaws.com';
+$user = 'admin';
+$password = '123456Yoram';
+$dbname = 'breached';
+
+// Connect to the database
+$conn = mysqli_connect($host, $user, $password, $dbname);
+if (!$conn) {
+    echo "<h1>Connection Error</h1>";
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Select data from breached_data table
+$sql = "SELECT id, email, pass, add_date FROM breached_data;";
+$result = mysqli_query($conn, $sql);
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+echo "<h1>Breached Data</h1>";
+echo "<table border='1'>";
+echo "<tr><th>ID</th><th>Email</th><th>Password</th><th>Date Added</th></tr>";
+
+// Output data of each row
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['id'] . "</td>";
+    echo "<td>" . $row['email'] . "</td>";
+    echo "<td>" . $row['pass'] . "</td>";
+    echo "<td>" . $row['add_date'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+
+// Close connection
+mysqli_close($conn);
+?>
+</body>
 </html>
